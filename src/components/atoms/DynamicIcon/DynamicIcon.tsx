1fbabf5950161147a874a547b805d6cb6df50295
sadpisco <leo99153@gmail.com>
2025-08-26 17:21:32 -0500

    Typography components is done

---
+++ src/components/atoms/DynamicIcon/DynamicIcon.tsx
@@ -0,0 +1,40 @@
+import { cn } from '@/lib/utils';
+import { lazy, useEffect, useState } from 'react';
+
+interface DynamicIconProps {
+    iconname?: string;
+    weight?: string;
+    className?: string;
+    size?: number;
+}
+
+interface IconProps {
+    className?: string;
+    weight?: string;
+    size?: number;
+}
+
+async function DynamicPhosphor(iconName: string) {
+    const module = await import(/* @vite-ignore */`@phosphor-icons/react/${iconName}`);
+    let result = { default: module[iconName] };
+    return result;
+};
+
+const DynamicIcon: React.FC<DynamicIconProps> = (props) => {
+    const { iconname, className, weight, size, ...rest } = props;
+
+    const [, setActive] = useState(false);
+    useEffect(() => {
+        setActive(true);
+    }, []);
+
+    if (iconname) {
+        const IconGenerator: React.FC<IconProps> = lazy(() =>
+            DynamicPhosphor(iconname.replace(/\s+/g, ''))
+        );
+        return <IconGenerator className={cn('', className)} weight={weight} size={size} {...rest} />;
+    }
+    return null;
+};
+
+export default DynamicIcon;
