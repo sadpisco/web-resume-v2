182a2ea3e6de02fde39719713b4804860cf2f6a8
sadpisco <leo99153@gmail.com>
2025-08-27 16:30:46 -0500

    Experience block is done

--- src/components/molecules/LangSwitch/LangSwitch.tsx
+++ src/components/molecules/LangSwitch/LangSwitch.tsx
@@ -14,9 +14,9 @@ const LangSwitch = ({ locale }: LangSwitchProps) => {
     }
     return (
         <section className="flex items-center gap-2 text-lg">
-            <span className="text-sm sm:text-lg">{locale === "en" ? "English" : "Inglés"}</span>
+            <span className="text-sm sm:text-md lg:text-lg">{locale === "en" ? "English" : "Inglés"}</span>
             <Switch checked={locale === "es"} onCheckedChange={handleChange} />
-            <span className="text-sm sm:text-lg">{locale === "en" ? "Spanish" : "Español"}</span>
+            <span className="text-sm sm:text-md lg:text-lg">{locale === "en" ? "Spanish" : "Español"}</span>
         </section>
     )
 }
